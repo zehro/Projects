06d085d6ac1394861c36035b1b0662b114b3eb8b
zehro <wisdom0music@gmail.com>
2017-03-25 21:53:16 -0400

    oh right, i put the twitter stuff in a folder now

--- zTo-Sort/Create-a-TwitterBot Workshop/Twitter Bot Example (alot-shepherd-master)/keys.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#keys.py
-
-keys = dict(
-    consumer_key =          '[YOUR CONSUMER KEY]',
-    consumer_secret =       '[YOUR CONSUMER SECRET]',
-    access_token =          '[YOUR ACCESS TOKEN]',
-    access_token_secret =   '[YOUR ACCESS TOKEN SECRET]',
-)
